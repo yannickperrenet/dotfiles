de66cbd66724b9bd638e7601640d2df63c387378
Yannick <yannick.perrenet@gmail.com>
2021-10-31 17:45:50 +0100

    Fix typo

--- .config/redshift/hooks/colorscheme.sh
+++ .config/redshift/hooks/colorscheme.sh
@@ -3,7 +3,7 @@ if [ "$1" = period-changed ]; then
     case $3 in
         daytime)
             ln -sf "$XDG_CONFIG_HOME"/alacritty/day.yml "$XDG_CONFIG_HOME"/alacritty/alacritty.yml
-            echo "daytime" > "$XDG_DATA_HOME"/redshift/current_period
+            echo "day" > "$XDG_DATA_HOME"/redshift/current_period
             ;;
         transition)
             if [ "$2" = none ]; then
